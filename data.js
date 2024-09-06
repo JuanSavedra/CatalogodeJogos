a1118240ca40bb3bf2264c822690817312aed62f
Juan <juansavedra827@gmail.com>
2024-09-06 19:26:43 -0300

    aula 3

---
+++ data.js
@@ -0,0 +1,17 @@
+let data = [
+    {
+        name: "Cuphead",
+        description: "Cuphead é um jogo eletrônico de run and gun e plataforma criado pelos irmãos canadenses Chad e Jared Moldenhauer através da Studio MDHR. O jogo foi inspirado no estilo de animação Rubber hose usado em desenhos animados da Era de Ouro da animação americana, como o trabalho dos estúdios Fleischer Studios, Warner Bros.",
+        gameOnSteam: "https://store.steampowered.com/app/460950/Katana_ZERO/"
+    },
+    {
+        name: "Katana Zero",
+        description: "Katana Zero é um jogo eletrônico de plataforma e ação bidimensional desenvolvido pela Askiisoft e publicado pela Devolver Digital para Microsoft Windows, macOS e Nintendo Switch em 2019.",
+        gameOnSteam: "https://store.steampowered.com/app/460950/Katana_ZERO/"
+    },
+    {
+        name: "Elden Ring",
+        description: "Elden Ring é um jogo eletrônico de RPG de ação em terceira pessoa, desenvolvido pela FromSoftware e publicado pela Bandai Namco Entertainment. O jogo é um projeto colaborativo entre o diretor Hidetaka Miyazaki e o romancista de fantasia George R. R. Martin.",
+        gameOnSteam: "https://store.steampowered.com/agecheck/app/1245620/"
+    }
+];
