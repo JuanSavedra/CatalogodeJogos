1cc4fb8fee22cdedf800ff67d3481b5484258eb7
Juan <juansavedra827@gmail.com>
2024-09-06 20:46:06 -0300

    aula 5

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 function Search() {
     let section = document.getElementById("resultados-pesquisa");
+    let searchField = document.getElementById("campo-pesquisa").value;
+    searchField = searchField.toLowerCase();
+
     let results = "";
+    let name = "";
+    let description = "";
+    let tags = "";
+
+    if (!searchField) {
+        section.innerHTML = `
+            <p>Lamentamos, não há nenhum resultado disponível.<p>
+        `
+        return;
+    }
 
     for (let data of datas) {
-        results += `
+        name = data.name.toLowerCase();
+        description = data.description.toLowerCase();
+        tags = data.tags.toLowerCase();
+
+        if (name.includes(searchField) || description.includes(searchField) || tags.includes(searchField)) {
+            results += `
             <div class="item-resultado">
                 <h2>${data.name}</h2>
                 <p class="descricao-meta">
@@ -12,6 +30,13 @@ function Search() {
                 <a href="${data.gameOnSteam}" target="_blank">Jogo na Steam</a>
             </div>
         `;
+        } 
+        
+        if (!results) {
+            results = `
+                <p>Lamentamos, não há nenhum resultado disponível.<p>
+            `
+        }
     }
 
     section.innerHTML = results;
--- data.js
+++ data.js
@@ -2,16 +2,19 @@ let datas = [
     {
         name: "Cuphead",
         description: "Cuphead é um jogo eletrônico de run and gun e plataforma criado pelos irmãos canadenses Chad e Jared Moldenhauer através da Studio MDHR. O jogo foi inspirado no estilo de animação Rubber hose usado em desenhos animados da Era de Ouro da animação americana, como o trabalho dos estúdios Fleischer Studios, Warner Bros.",
-        gameOnSteam: "https://store.steampowered.com/app/460950/Katana_ZERO/"
+        gameOnSteam: "https://store.steampowered.com/app/460950/Katana_ZERO/",
+        tags: "indie plataforma difícil dificil desenho animado cooperativo 2D 2d cartoon cartunizado engraçado"
     },
     {
         name: "Katana Zero",
         description: "Katana Zero é um jogo eletrônico de plataforma e ação bidimensional desenvolvido pela Askiisoft e publicado pela Devolver Digital para Microsoft Windows, macOS e Nintendo Switch em 2019.",
-        gameOnSteam: "https://store.steampowered.com/app/460950/Katana_ZERO/"
+        gameOnSteam: "https://store.steampowered.com/app/460950/Katana_ZERO/",
+        tags: "indie plataforma pixel pixelado ação acao difícil dificil violento 2D 2d hack slash ficção ficcao ninja"
     },
     {
         name: "Elden Ring",
         description: "Elden Ring é um jogo eletrônico de RPG de ação em terceira pessoa, desenvolvido pela FromSoftware e publicado pela Bandai Namco Entertainment. O jogo é um projeto colaborativo entre o diretor Hidetaka Miyazaki e o romancista de fantasia George R. R. Martin.",
-        gameOnSteam: "https://store.steampowered.com/agecheck/app/1245620/"
+        gameOnSteam: "https://store.steampowered.com/agecheck/app/1245620/", 
+        tags: "ação acao rpg fantasia soulslike cooperativo violento 3D 3d sonora pvp difícil dificil terceira pessoa aaa"
     }
 ];
